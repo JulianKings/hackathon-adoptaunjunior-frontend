f25610c737220f180891be3a9f85ccaeaae066a8
Alejandro <alejandroluisacosta95@gmail.com>
2024-10-26 13:32:56 +0200

    header styles finished

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,23 +6,23 @@ export function Header() {
         <header className="header">
             <div className="header__container">
                 <div className="header__link-container">
-                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <img className="header__icon header__icon--skull" src="/src/assets/skull.png" />
                     <NavLink className="header__page-link" to='/'>Inicio</NavLink>
                 </div>
                 <div className="header__link-container">
-                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <img className="header__icon" src="/src/assets/magic-wand.png" />
                     <NavLink className="header__page-link" to='/challenges'>Trucos</NavLink>
                 </div>
                 <div className="header__link-container">
-                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <img className="header__icon" src="/src/assets/lollipop.png" />
                     <NavLink className="header__page-link" to='/resources'>Tratos</NavLink>
                 </div>
                 <div className="header__link-container">
-                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <img className="header__icon" src="/src/assets/account.png" />
                     <NavLink className="header__page-link" to='/signup'>Registro</NavLink>
                 </div>
                 <div className="header__link-container">
-                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <img className="header__icon" src="/src/assets/login.png" />
                     <NavLink className="header__page-link" to='/login'>Login</NavLink>
                 </div>
             </div>
