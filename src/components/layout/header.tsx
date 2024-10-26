05fa1dc06a0e0a64625b5820ca916d9f8a4b5661
Alejandro <alejandroluisacosta95@gmail.com>
2024-10-26 13:12:52 +0200

    add header filled with link containers

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,18 +4,28 @@ import { NavLink } from "react-router-dom";
 export function Header() {
     return (
         <header className="header">
-            <div className="header__link-container">
-                <img className="header__icon" src="/src/assets/skull.png" />
-                <NavLink to='/'>Inicio</NavLink>
+            <div className="header__container">
+                <div className="header__link-container">
+                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <NavLink className="header__page-link" to='/'>Inicio</NavLink>
+                </div>
+                <div className="header__link-container">
+                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <NavLink className="header__page-link" to='/challenges'>Trucos</NavLink>
+                </div>
+                <div className="header__link-container">
+                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <NavLink className="header__page-link" to='/resources'>Tratos</NavLink>
+                </div>
+                <div className="header__link-container">
+                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <NavLink className="header__page-link" to='/signup'>Registro</NavLink>
+                </div>
+                <div className="header__link-container">
+                    <img className="header__icon" src="/src/assets/skull.png" />
+                    <NavLink className="header__page-link" to='/login'>Login</NavLink>
+                </div>
             </div>
-             |  
-            <NavLink to='/challenges'>Trucos</NavLink>
-             | 
-            <NavLink to='/resources'>Tratos</NavLink>
-             | 
-            <NavLink to='/signup'>Sign Up</NavLink>
-             | 
-            <NavLink to='/login'>Login</NavLink>
         </header>
     );
 }
