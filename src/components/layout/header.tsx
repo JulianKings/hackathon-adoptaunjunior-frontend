9f91fec53f999d2bf82a4f08d10a7b9935ef686b
Alejandro <alejandroluisacosta95@gmail.com>
2024-10-26 13:44:54 +0200

    add icons turned into page links

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,23 +6,23 @@ export function Header() {
         <header className="header">
             <div className="header__container">
                 <div className="header__link-container">
-                    <img className="header__icon header__icon--skull" src="/src/assets/skull.png" />
+                    <NavLink to="/"><img className="header__icon header__icon--skull" src="/src/assets/skull.png" /></NavLink>
                     <NavLink className="header__page-link" to='/'>Inicio</NavLink>
                 </div>
                 <div className="header__link-container">
-                    <img className="header__icon" src="/src/assets/magic-wand.png" />
+                    <NavLink to="/challenges"><img className="header__icon" src="/src/assets/magic-wand.png" /></NavLink>
                     <NavLink className="header__page-link" to='/challenges'>Trucos</NavLink>
                 </div>
                 <div className="header__link-container">
-                    <img className="header__icon" src="/src/assets/lollipop.png" />
+                    <NavLink to="/resources"><img className="header__icon" src="/src/assets/lollipop.png" /></NavLink>
                     <NavLink className="header__page-link" to='/resources'>Tratos</NavLink>
                 </div>
                 <div className="header__link-container">
-                    <img className="header__icon" src="/src/assets/account.png" />
+                    <NavLink to="/signup"><img className="header__icon" src="/src/assets/account.png" /></NavLink>
                     <NavLink className="header__page-link" to='/signup'>Registro</NavLink>
                 </div>
                 <div className="header__link-container">
-                    <img className="header__icon" src="/src/assets/login.png" />
+                    <NavLink to="/login"><img className="header__icon" src="/src/assets/login.png" /></NavLink>
                     <NavLink className="header__page-link" to='/login'>Login</NavLink>
                 </div>
             </div>
