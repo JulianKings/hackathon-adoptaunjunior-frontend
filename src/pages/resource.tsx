aa2136c26189453066e7a2c0a555b3153e6c90a9
LuLuMS0206 <luciamacho00@gmail.com>
2024-10-28 11:30:55 +0100

    add resource

--- src/pages/resource.tsx
+++ src/pages/resource.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./../style/pages/layout/resource.scss";
+
+const resources = [
+    {
+        id: "1",
+        title: "¿Ejercicios o proyectos?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "/assets/resource-mock-image.png"
+    },
+];
 
 export function Resource() {
     const { id } = useParams<{ id: string }>();
 
+    const resource = resources.find((res) => res.id === id);
+
+    if (!resource) {
+        return <p>Resource not found</p>;
+    }
+
     return (
-        <div>
-            Resource Page for id: {id}
+        <div className="resource">
+            <h1 className="resource__title">{resource.title}</h1>
+            <img
+                src={resource.image}
+                alt={resource.title}
+                className="resource__image"
+            />
+            <p className="resource__text">{resource.description}</p>
         </div>
     );
 }
