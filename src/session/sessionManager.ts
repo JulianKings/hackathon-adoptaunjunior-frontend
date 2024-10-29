70cc8453a6877d10bb15a25798ae4fa4b73fdee4
JulianKings <julli123@hotmail.es>
2024-10-29 02:31:02 +0100

    fix: env fixes

--- src/session/sessionManager.ts
+++ src/session/sessionManager.ts
@@ -2,6 +2,7 @@ import { LocalStorageLoginInformation, LoginAttempt } from "../interfaces/sessio
 import { ApiUserInterface } from "../interfaces/user";
 
 const path = (process.env.NODE_ENV !== 'production') ? import.meta.env.VITE_API_LOCATION : process.env.REACT_APP_API_LOCATION;
+const vite = import.meta.env.VITE_API_LOCATION;
 
 export async function getSession(ssoToken?: LocalStorageLoginInformation): Promise<ApiUserInterface | null> {
     if(ssoToken && ssoToken.jwt_token)  {
@@ -42,6 +43,52 @@ export async function getSession(ssoToken?: LocalStorageLoginInformation): Promi
 }
 
 export async function attemptLogin(email: string, password: string): Promise<LoginAttempt | null> {
+    console.error(process.env.NODE_ENV);
+    console.error(vite);
+    
+    return fetch(path + "login", { 
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        mode: "cors",
+        body: JSON.stringify({ email: email, password: password }),
+    })
+    .then((response) => {
+        if (response.status >= 400) {
+            throw new Error("server error");
+        }
+
+        return response.json();
+    })
+    .then((response) => {
+        console.log(response);
+        if(response.responseStatus)
+        {
+            if(response.responseStatus === 'validLogin')
+            {
+                // Do JWT stuff
+                localStorage.setItem('sso_token', JSON.stringify({jwt_token: response.token}));
+                const result: LoginAttempt = {
+                    status: 'valid',
+                }
+                return result;
+            } else {
+                const result: LoginAttempt = {
+                    status: 'invalid',
+                    errors: response.errors
+                }
+                return result;
+            }
+    
+        }    
+        
+        return null;
+    })
+    .catch((error) => {
+        throw new Error(error);
+    });
+}
+
+export async function attemptRegister(email: string, password: string, name: string, experience: string): Promise<LoginAttempt | null> {
     return fetch(path + "login", { 
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
