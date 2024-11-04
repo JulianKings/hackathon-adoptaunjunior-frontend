7c0ab6287f24ca933c7da19b0a3df6bcb32f7f35
Alejandro <alejandroluisacosta95@gmail.com>
2024-11-04 17:58:11 +0100

    add: shadow effect for current page (only homepage)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { useApiSelector } from "../../redux/store";
 import { selectSession } from "../../redux/slices/session";
@@ -9,37 +9,45 @@ export function Header() {
     const headerClass = location.pathname === '/' ? 'header--home' : 'header--default';
     const session = useApiSelector(selectSession);
 
+    const getShadowClass = (path) => location.pathname === path ? 'shadow-active' : '';
+
     const handleHover = (event) => {
-        event.currentTarget.style.boxShadow = '0 4px 8px rgba(255, 255, 255, 0.6)';
-        event.currentTarget.style.borderRadius = '12px';
+        const target = event.currentTarget as HTMLElement;
+    
+        if (!target.classList.contains('shadow-active')) {
+            target.style.boxShadow = location.pathname === '/' ? 
+                '0 4px 8px rgba(255, 255, 255, 0.6)' : 
+                '0 4px 8px rgba(0, 0, 0, 0.6)';
+        }
     };
     
     const handleHoverLeave = (event) => {
-        event.currentTarget.style.boxShadow = 'none';
+        const target = event.currentTarget as HTMLElement;
+            event.currentTarget.style.boxShadow = 'inherit';
     };
 
     return (
         <header className={`header ${headerClass}`}>
             <div className="header__container">
-                <div className="header__link-container" onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
+                <div className={`header__link-container ${getShadowClass('/')}`} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
                     <NavLink to="/">
                         <img className="header__icon header__icon--skull" src="/src/assets/skull.png" />
                     </NavLink>
                     <NavLink className="header__page-link" to='/'>Inicio</NavLink>
                 </div>
-                <div className="header__link-container" onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
+                <div className={`header__link-container ${getShadowClass('/challenges')}`} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
                     <NavLink to="/challenges">
                         <img className="header__icon" src="/public/assets/magic.svg" />
                     </NavLink>
                     <NavLink className="header__page-link" to='/challenges'>Trucos</NavLink>
                 </div>
-                <div className="header__link-container" onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
+                <div className={`header__link-container ${getShadowClass('/resources')}`} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
                     <NavLink to="/resources">
                         <img className="header__icon" src="/public/assets/lollipop.svg" />
                     </NavLink>
                     <NavLink className="header__page-link" to='/resources'>Tratos</NavLink>
                 </div>
-                <div className="header__link-container" onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
+                <div className={`header__link-container ${getShadowClass('/help')}`} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
                     <NavLink to="/help">
                         <img className="header__icon" src="/src/assets/help.png" />
                     </NavLink>
@@ -47,7 +55,7 @@ export function Header() {
                 </div>
                 {(session) ?
                     <Fragment>                        
-                        <div className="header__link-container" onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
+                        <div className={`header__link-container ${getShadowClass('/logout')}`} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
                             <NavLink to="/logout">
                                 <img className="header__icon" src="/public/assets/login.svg" />
                             </NavLink>
@@ -55,7 +63,7 @@ export function Header() {
                         </div>
                     </Fragment> :
                     <Fragment>
-                        <div className="header__link-container" onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
+                        <div className={`header__link-container ${getShadowClass('/login')}`} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
                             <NavLink to="/login">
                                 <img className="header__icon" src="/public/assets/login.svg" />
                             </NavLink>
